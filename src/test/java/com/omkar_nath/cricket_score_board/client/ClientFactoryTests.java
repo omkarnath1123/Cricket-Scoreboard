c2ad95614e25a38816e6a5a73379b7ff213e7c6d
omkarnath1123 <9818271889Abc>
2022-03-06 23:55:52 +0530

    update and added Inning

--- src/test/java/com/omkar_nath/cricket_score_board/client/ClientFactoryTests.java
+++ src/test/java/com/omkar_nath/cricket_score_board/client/ClientFactoryTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ClientFactoryTests {
@@ -21,10 +22,23 @@ public class ClientFactoryTests {
     }
 
     @Test
-    public void buildClientWithValidFilePath_shouldCreateFileClient() throws FileNotFoundException, IOException {
+    public void buildClientWithNoArg() throws FileNotFoundException {
+        String[] args = {};
+        Client client = ClientFactory.buildClient(args, commandFactory);
+        assertTrue(client instanceof CliClient);
+    }
+
+    @Test
+    public void buildClientWithValidFilePath() throws FileNotFoundException, IOException {
         String[] args = {"file_input.txt"};
         Client client = ClientFactory.buildClient(args, commandFactory);
         client.handleInput();
         assertTrue(client instanceof FileClient);
+    }
+
+    @Test
+    public void buildClientWithInvalidFilePath() throws FileNotFoundException {
+        String[] args = {"invalid_file_path.txt"};
+        assertThrows(FileNotFoundException.class, () -> ClientFactory.buildClient(args, commandFactory));
     }
 }
